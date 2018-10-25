e345f05f8a036d170351fbd783aeefc9211fcdcd
jiangyanxin <jiangyanxin1989@gmail.com>
2018-10-25 17:50:31 +0800

    Updates

---
+++ precache-manifest.2eb0c1f3e79caa192f15ce3d789b992c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f96386fd9f7f1363649",
+    "url": "/search-nes/static/js/runtime~main.2f96386f.js"
+  },
+  {
+    "revision": "690dc523742506a76068",
+    "url": "/search-nes/static/js/main.690dc523.chunk.js"
+  },
+  {
+    "revision": "5693639ba3fbd9f0ab0d",
+    "url": "/search-nes/static/js/1.5693639b.chunk.js"
+  },
+  {
+    "revision": "690dc523742506a76068",
+    "url": "/search-nes/static/css/main.40b42d51.chunk.css"
+  },
+  {
+    "revision": "9da9cf5d4c07ff42a54767ce06890a94",
+    "url": "/search-nes/index.html"
+  }
+];
