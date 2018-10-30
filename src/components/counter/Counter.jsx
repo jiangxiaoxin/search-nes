5aa3b114bb50b82e9be23d9918af048dc7466be4
jiangxiaoxin <jiangyanxin1989@gmail.com>
2018-10-30 14:46:42 +0800

    add counter part

---
+++ src/components/counter/Counter.jsx
@@ -0,0 +1,1 @@
+import React, {Component} from "react";export default class Counter extends Component{    constructor(props) {        super(props)    }    render() {        return (            <div>            </div>        )    }}
