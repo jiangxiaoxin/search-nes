b1a3bff3e70fcace7e1851fe08fa526642bd87c4
jiangxiaoxin <jiangyanxin1989@gmail.com>
2018-10-30 14:27:44 +0800

    set up project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import Welcome from './components/welcome/Welcome.jsx';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Welcome />
       </div>
     );
   }
